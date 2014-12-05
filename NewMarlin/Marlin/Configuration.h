cb88a9c1f2644f4166ade6f9b33e9312677e80a6
Marco van Noord <marcovannoord+sourcetree@gmail.com>
2014-12-05 16:04:12 +0100

    Updated steps/mm

--- NewMarlin/Marlin/Configuration.h
+++ NewMarlin/Marlin/Configuration.h
@@ -496,7 +496,7 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 
 // default settings
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,533.3,1000}  // default steps per unit for 20tooth belts
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {80,80,533.3,647}  // default steps per unit for 20tooth belts
 #define DEFAULT_MAX_FEEDRATE          {175, 175, 5, 25}    // (mm/sec)    
 #define DEFAULT_MAX_ACCELERATION      {4000,4000,20,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
